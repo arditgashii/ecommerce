2854589f1316be539f81178a9cc89af3f73e0908
gashiardit <arditfcb58@gmail.com>
2024-02-17 18:12:36 +0100

    Added function to add a new user, connected with MongoDB database, which save's user's data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,2 +1,30 @@
 import User from '../models/userModel.js'
+import asyncHandler from '../middlewares/asyncHandler.js'
 
+const createUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    
+    if (!username || !email || !password) {
+        throw new Error("Please fill all the inputs.");
+    }
+
+    const userExists = await User.findOne({email})
+    if (userExists) res.status(400).send("User already exists!");
+
+    const newUser = new User({ username, email, password })
+
+    try {
+        await newUser.save()
+        res.status(201).json({
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+        });
+    } catch (error) {
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+});
+
+export {createUser};
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 
 import connectDB from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 const port = process.env.PORT || 5000;
