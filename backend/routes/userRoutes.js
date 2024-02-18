4f106f0c7936a5596cbc67dc1bbe228fed16cddf
gashiardit <arditfcb58@gmail.com>
2024-02-18 04:27:12 +0100

    Update Current profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import { createUser, 
          loginUser,
          logoutCurrentUser,
-         getAllUsers } 
+         getAllUsers,
+         getCurrentUserProfile,
+         updateCurrentUserProfile } 
          from "../controllers/userController.js";
 
 
@@ -11,11 +13,15 @@ import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
+router.route("/").
+post(createUser).
+get(authenticate, authorizeAdmin, getAllUsers);
+
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
 
-
+router.route("/profile").get(authenticate, getCurrentUserProfile)
+                        .put(authenticate, updateCurrentUserProfile);
 
 
 export default router;
