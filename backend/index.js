f0625fabe64f7f94819c182809d5d6e8802d2f4a
gashiardit <arditfcb58@gmail.com>
2024-02-20 14:09:29 +0100

    Added controller, route and model for Category, connected Category with mongoDB, tested adding a new category

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser'
 
 import connectDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
+import categoryRoutes from './routes/categoryRoutes.js'
 
 dotenv.config()
 const port = process.env.PORT || 5000;
@@ -18,5 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
+app.use('/api/category', categoryRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
