fe440b0fae1a95fddd773971e1fb5d9dc885cc41
fatjonmulaj <fatjonm20@gmail.com>
2024-02-23 03:17:25 +0100

    new commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -25,6 +25,7 @@ import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
 import AllProducts from './pages/Admin/AllProducts.jsx';
 import Home from './Home.jsx';
 import Favorites from './pages/Products/Favorites.jsx';
+import ProductDetails from './pages/Products/ProductDetails.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,6 +34,7 @@ const router = createBrowserRouter(
       <Route path='/register' element={<Register />} /> 
       <Route index={true} path='/' element={<Home />} /> 
       <Route path='/favorite' element={<Favorites />} />
+      <Route path='/product/:id' element={<ProductDetails />} />
 
 
       <Route path="" element={<PrivateRoute />}>
