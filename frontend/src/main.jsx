6dd013b3b2257fbeccd5b3b2479cdb756ea0336e
Rinor Guri <rinorguri56@gmail.com>
2024-02-25 00:05:44 +0100

    Order,Payment and Shipping

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,32 +29,33 @@ import ProductDetails from './pages/Products/ProductDetails.jsx';
 
 import Cart from "./pages/Cart.jsx";
 import Shop from "./pages/Shop.jsx";
+import Shipping from './pages/Orders/Shipping.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} /> 
-      <Route index={true} path='/' element={<Home />} /> 
-      <Route path='/favorite' element={<Favorites />} />
-      <Route path='/product/:id' element={<ProductDetails />} />
-      <Route path='/cart' element={<Cart />} /> 
-      <Route path='/shop' element={<Shop />} /> 
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/favorite" element={<Favorites />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/shop" element={<Shop />} />
 
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
+        <Route path="/shipping" element={<Shipping />} />
       </Route>
 
-     {/*Admin Routes  */}
+      {/*Admin Routes  */}
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
-        <Route path="categorylist" element={<CategoryList/>} />
+        <Route path="categorylist" element={<CategoryList />} />
         <Route path="productlist" element={<ProductList />} />
         <Route path="productlist/:pageNumber" element={<ProductList />} />
         <Route path="allproductslist" element={<AllProducts />} />
         <Route path="product/update/:_id" element={<ProductUpdate />} />
       </Route>
-
     </Route>
   )
 );
