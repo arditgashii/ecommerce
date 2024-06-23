0dfd615dcfb4d154ea5a4fefc4960844d203a9c7
gashiardit <arditfcb58@gmail.com>
2024-06-23 18:15:04 +0200

    changes on index and json

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
@@ -10,7 +10,7 @@ import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from './routes/uploadRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 
-dotenv.config()
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -31,7 +31,7 @@ app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname + "/uploads")));
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
