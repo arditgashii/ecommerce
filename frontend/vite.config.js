533e94b5a7962ef5a346d1a5ffd48b16e9f5f270
gashiardit <arditfcb58@gmail.com>
2024-06-23 18:57:57 +0200

    changes on vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -14,10 +14,12 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'] // Example: Create a separate chunk for vendor libraries
+          vendor: ['react', 'react-dom'] // Separate chunk for vendor libraries
         }
       }
     },
     chunkSizeWarningLimit: 1000 // Increase limit to 1000 kB
-  }
-})
+  },
+  root: './', // Ensure this points to the root of your project
+  publicDir: './public' // Path to your public directory containing index.html
+});
